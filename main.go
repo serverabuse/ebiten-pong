12e67df2ee8b8a7b539d60816d9c18b4316a9e0b
Carmelo Riolo <carmeloriolo93@gmail.com>
2022-05-15 01:53:02 +0200

    Moved function in util.go

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"golang.org/x/image/colornames"
-	"image/color"
 	"log"
-	"math"
 	"os"
 )
 
@@ -35,21 +33,6 @@ type Game struct {
 	ball            *Ball
 	p1Score         int
 	cpuScore        int
-}
-
-func drawCircle(screen *ebiten.Image, x, y, radius int, clr color.Color) {
-	radius64 := float64(radius)
-	minAngle := math.Acos(1 - 1/radius64)
-
-	for angle := float64(0); angle <= 360; angle += minAngle {
-		xDelta := radius64 * math.Cos(angle)
-		yDelta := radius64 * math.Sin(angle)
-
-		x1 := int(math.Round(float64(x) + xDelta))
-		y1 := int(math.Round(float64(y) + yDelta))
-
-		screen.Set(x1, y1, clr)
-	}
 }
 
 func (g *Game) Reset() {
---
+++ util.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"math"
+)
+
+func drawCircle(screen *ebiten.Image, x, y, radius int, clr color.Color) {
+	radius64 := float64(radius)
+	minAngle := math.Acos(1 - 1/radius64)
+
+	for angle := float64(0); angle <= 360; angle += minAngle {
+		xDelta := radius64 * math.Cos(angle)
+		yDelta := radius64 * math.Sin(angle)
+
+		x1 := int(math.Round(float64(x) + xDelta))
+		y1 := int(math.Round(float64(y) + yDelta))
+
+		screen.Set(x1, y1, clr)
+	}
+}
